ca9afbe02dbf144e50cc134d0b60ceaa7aa2f99c
Carlos Quintero <CarlosQuintero92.cq@gmail.com>
2021-06-22 23:39:43 +0200

    creating a component button to navigate the web

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { useHistory } from "react-router-dom";
+// import { connect } from "react-redux";
+// import { LOGOUT } from "../../redux/type";
+import Boton from "../Boton/Boton";
 
-const Header = () => {
-    return (
-        <div className="headerBody">
-            <div className="header"></div>
-            Hoy el Header
+const Header = (props) => {
+
+
+    let history = useHistory();
+    const takeMe = (were) =>{
+        history.push(were)
+    }
+
+
+
+
+
+    return ( 
+        <div className="headerBody" >
+        <Boton lugar="/login" destino="Login" onClick={() => takeMe("/")}/>
+        <Boton lugar="/register" destino="register" onClick={() => takeMe("/")}/>
+        <Boton lugar="/" destino="home" onClick={() => takeMe("/")}/>
         </div>
-    )
+    )   
 }
 
 export default Header
