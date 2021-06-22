57246884640b1681531722c6238695638d24163d
Carlos Quintero <CarlosQuintero92.cq@gmail.com>
2021-06-22 17:02:27 +0200

    Creating a working environment creating the basic form of the application installing redux and scss

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import './App.scss';
+import Footer from './Components/Footers/Footer';
+import Header from './Components/Header/Header';
+import Home from './Containers/Home/Home';
+import Login from './Containers/Login/Login';
+import Register from './Containers/Register/Register';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <BrowserRouter>
+
+      <Header/>
+      <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path="/login" exact component={Login} />
+        <Route path="/register" exact component={Register}/>
+      </Switch>
+      <Footer/>
+      
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="headerBody">
+            <div className="header"></div>
+            Hoy el Header
+        </div>
+    )
+}
+
+export default Header
