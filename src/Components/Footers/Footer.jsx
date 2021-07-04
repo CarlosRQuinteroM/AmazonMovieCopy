384da2dda1386eabc9f058988a292726a6cc6aa2
Carlos Quintero <CarlosQuintero92.cq@gmail.com>
2021-07-04 20:44:26 +0200

    create de footers add and hiperlink the icons

--- src/Components/Footers/Footer.jsx
+++ src/Components/Footers/Footer.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import"./Footer.scss"
+import { FaGithub , FaLinkedin ,FaRegCopyright } from 'react-icons/fa';
+
+
+
+
 
 const Footer = () => {
+     let history = useHistory()
+
     return (
-        <div>
-            Footer
+        <div className="footerBody">
+            <div className="SocialMedia">
+            <a href="https://github.com/CarlosRQuinteroM" Target="_blank" ><FaGithub id="github" className="socialIcon"/></a>
+            <a href="https://www.linkedin.com/in/carlos-quintero-moreno/"  Target="_blank" ><FaLinkedin  id="linkInd" className="socialIcon"/></a>
+            </div>
+           <FaRegCopyright/> By Carlos Quintero Moreno 2021 
         </div>
     )
 }
